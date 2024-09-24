25b602e36136211a8937f5464e262eb8f0bacf54
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2024-09-24 10:36:01 -0300

    arrumando o acesso a cadastro

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,19 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import Header from "../components/Header";
 import useContexts from "../hooks/useContext";
 import HeaderMobile from "../components/HeaderMobile";
 import Footer from "../components/Footer";
 
 export default function RootLayout() {
-    const { isMobileBig } = useContexts()
+  const { isMobileBig } = useContexts();
+  const navigate = useNavigate();
 
-    return (
-        <>
-            {isMobileBig ? <HeaderMobile /> : <Header /> }
-            <main>
-                <Outlet />
-            </main>
-            <Footer />
-        </>
-    )
+  useEffect(() => {
+    // Verifica se há um login armazenado no sessionStorage
+    const loggedIn = sessionStorage.getItem("login");
+
+    // Se não houver, redireciona para a página desejada
+    if (!loggedIn) {
+      navigate("/"); // ou outra rota que você queira
+    }
+  }, [navigate]);
+
+  return (
+    <>
+      {isMobileBig ? <HeaderMobile /> : <Header />}
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 }
